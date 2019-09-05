913b400d0f21a698f45955052cb4530f2c5c4eaa
adharsh8 <adharsh.s@psiog.com>
2019-09-05 14:40:14 +0530

    Update report.js

--- report.js
+++ report.js
@@ -1,45 +1,42 @@
-var stuID = new Array;
+
 var no_of_sub = 4;
 var grade = new Array;
-var div = new Array;
-var english = new Array;
-var maths = new Array;
-var science = new Array;
-var social = new Array;
-var sum = new Array;
-var average = new Array;
-var pass = ["0","0","0","0","0","0","0","0","0","0","0","0","0"];
-var fail = ["0","0","0","0","0","0","0","0","0","0","0","0","0"];
-var no_of_stud = ["0","0","0","0","0","0","0","0","0","0","0","0","0"];
-var avg = 0;
-var x = 0;
+var sum = 0; var english = 0; var maths = 0; var science = 0; var social = 0;
+var average = 0;
+var pass = new Array(13).fill(0);
+var fail = new Array(13).fill(0);
+var no_of_stud = new Array(13).fill(0);
 
 function Add_element_in_array()
 {
     
-    stuID[x] = parseInt(document.getElementById("ID").value,10);
+    stuID = parseInt(document.getElementById("ID").value,10);
     grade[x] = parseInt(document.getElementById("grade").value,10);
-    div[x] = parseInt(document.getElementsByName("div").value,10);
-    english[x] = parseInt(document.getElementById("english").value,10);
-    maths[x] = parseInt(document.getElementById("maths").value,10);
-    science[x] = parseInt(document.getElementById("science").value,10);
-    social[x] = parseInt(document.getElementById("social").value,10);
+    div = parseInt(document.getElementsByName("div").value,10);
+    english = parseInt(document.getElementById("english").value,10);
+    maths = parseInt(document.getElementById("maths").value,10);
+    science = parseInt(document.getElementById("science").value,10);
+    social = parseInt(document.getElementById("social").value,10);
     
-    validate();
-}
-function validate()
-{
-    
-    if((stuID[x]=="")||(grade[x]=="")||(div[x]==""))
-    {
-        alert("Enter every Credentials..");
-    }
-    else if((english[x]>100)||(maths[x]>100)||(science[x]>100)||(social[x]>100))
+   if(isNaN(stuID)){
+       alert("Enter the Student ID ");
+   }
+   else if(stuID<0){
+    alert("enter valid Student ID");
+   }
+   else if(isNaN(grade[x])){
+       alert("Select the Grade");
+   }
+   else if(isNaN(english)||isNaN(maths)||isNaN(science)||isNaN(social))
+   {
+       alert("Enter every records..");
+   }
+    else if((english>100)||(maths>100)||(science>100)||(social>100))
     {
         alert("Marks limit should not exceed 100...");
         clear_marks();
     }
-    else if((english[x]<0)||(maths[x]<0)||(science[x]<0)||(social[x]<0))
+    else if((english<0)||(maths<0)||(science<0)||(social<0))
     {
         alert("Please Enter the valid mark...");
         clear_marks();
@@ -70,12 +67,12 @@ function clear_marks()
 function evaluate()
 {
     alert("Marks Entered...");
-    sum[x]=english[x]+maths[x]+science[x]+social[x];
-    alert("Test" + sum[x]);
-    average[x]=sum[x]/no_of_sub;
-    if(average[x]>=60)
+    sum=english+maths+science+social;
+    average=sum/no_of_sub;
+    var x = 0;
+    if(average>=60)
     {
-        pass[x]++;
+        pass[grade[x]]++;
         no_of_stud[x]++;
     }
     else
@@ -84,34 +81,38 @@ function evaluate()
         no_of_stud[grade[x]]++;
     }
     x++;
-    alert(x);
     clear_complete();
 }
+var entry=document.getElementById("button2");
+entry.addEventListener("click",display());
 function display()
 {
-    alert("result will be displayed...");
-    
-    var h = "Grade  No.of.Students  PassStudents  FailStudents  Average";
-    var r,l;
-    var b="&nbsp";
-    for(var i=1;i<25;i++)
-    {
-      l+=b;  
-    }
+    var row=1;
+    var avg = 0;
     for(var i=1;i<=12;i++)
     {
         if(no_of_stud[i] !=0)
         {
-          avg = ((pass[i]/no_of_stud[i])*100);
-          r += b + grade[i] + l + no_of_stud[i] + l + pass[i] + l + fail[i] + l + avg.toFixed(2) + "<br/>";
-
+            avg=(pass[i]/no_of_stud[i]);
         }
         else
         {
-            r+= b + grade[i] + l + no_of_stud[i] + l + pass[i] + l + fail[i] + l + "0.0" + "<br/>"; 
+            avg=0.0;
         }
-       
+        var table = document.getElementById("table");
+        var newRow = table.insertRow(row);
+        var cell1 = newRow.insertCell(0);
+        var cell2 = newRow.insertCell(1);
+        var cell3 = newRow.insertCell(2);
+        var cell4 = newRow.insertCell(3);
+        var cell5 = newRow.insertCell(4);
+
+        cell1.innerHTML= grade[i];
+        cell2.innerHTML= no_of_stud[i];
+        cell3.innerHTML= pass[i];
+        celll4.innerHTML= fail[i];
+        cell5.innerHTML= avg[i];
+        row++;
     }
-    document.getElementById("Result").innerHTML=h+r;
- 
+    
 }
