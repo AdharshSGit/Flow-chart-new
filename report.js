7bd32daf0095bce28aa80afd5202ca4522d7a58e
adharsh8 <adharsh.s@psiog.com>
2019-11-27 19:04:14 +0530

    Update report.js

--- report.js
+++ report.js
@@ -81,54 +81,38 @@ function evaluate()
     
     clear_complete();
 }
+
 function display()
 {
-    var Table = "<h1><br>Academic Final Report<br><br><hr></h1>";
-    Table += "<table ><tr><td style= 'width: 100px; height: 30px; text-align: centre; font-weight: bolder;'>Grades<hr></td>";
-    Table += "<td style='width: 100px; height: 30px; text-align: centre;font-weight: bolder;'>No of students<hr></td>";
-    Table += "<td style='width: 100px; height: 30px; text-align: centre;font-weight: bolder;'>Pass_Students<hr></td>";
-    Table += "<td style='width: 100px; height: 30px; text-align: centre;font-weight: bolder;'>Fail_Students<hr></td>";
-    Table += "<td style='width: 100px; height: 30px; text-align: centre;font-weight: bolder;'>Average<hr></td></tr>";
+    var Table = "<h1><br>Academic Final Report<br><br></h1>";
+    Table += "<table ><tr><td style= 'width: 100px; height: 30px; text-align: center; font-weight: bolder;background: lightblue;'>Grades</td>";
+    Table += "<td style='width: 100px; height: 30px; text-align: center;font-weight: bolder;background: lightblue;'>No of students</td>";
+    Table += "<td style='width: 100px; height: 30px; text-align: center;font-weight: bolder;background: lightblue;'>Pass_Students</td>";
+    Table += "<td style='width: 100px; height: 30px; text-align: center;font-weight: bolder;background: lightblue;'>Fail_Students</td>";
+    Table += "<td style='width: 100px; height: 30px; text-align: center;font-weight: bolder;background: lightblue;'>Average</td></tr>";
 
 
     for (var i = 1; i <= 12; i++) {
 
-       Table += "<tr><td style='width: 100px; text-align:centre;'>" + i + "</td>";
-       Table += "<td style='width: 100px; text-align: centre;'>" + no_of_stud[i] + "</td>";
-       Table += "<td style='width: 100px; text-align: centre;'>" + pass[i] + "</td>";
-       Table += "<td style='width: 100px; text-align: centre;'>" + fail[i] + "</td>";
-       Table += "<td style='width: 100px; text-align: centre;'>" + passpercent[i] + "</td></tr>";
+       Table += "<tr><td style='width: 100px; text-align:center;'>" + i + "</td>";
+       Table += "<td style='width: 100px; text-align: center;'>" + no_of_stud[i] + "</td>";
+       Table += "<td style='width: 100px; text-align: center;'>" + pass[i] + "</td>";
+       Table += "<td style='width: 100px; text-align: center;'>" + fail[i] + "</td>";
+       Table += "<td style='width: 100px; text-align: center;'>" + passpercent[i] + "</td></tr>";
 
     }
     
     
-    Table += "<tr><td style='width: 100px;height: 30px; text-align:centre;font-weight: bolder;background: lightslategray;'>" + "ALL" + "</td>";
-    Table += "<td style='width: 100px; height: 30px;text-align: centre;font-weight: bolder;background: lightslategray;'>" + totalstudents + "</td>";
-    Table += "<td style='width: 100px; height: 30px;text-align: centre;font-weight: bolder;background: lightslategray;'>" + totalpass + "</td>";
-    Table += "<td style='width: 100px; height: 30px;text-align: centre;font-weight: bolder;background: lightslategray;'>" + totalfail + "</td>";
-    Table += "<td style='width: 100px; height: 30px;text-align: centre;font-weight: bolder;background: lightslategray;'>" + totalpasspercent + "</td></tr>";
+    Table += "<tr><td style='width: 100px;height: 30px; text-align:center;font-weight: bolder;background: lightblue;'>" + "ALL" + "</td>";
+    Table += "<td style='width: 100px; height: 30px;text-align: center;font-weight: bolder;background: lightblue;'>" + totalstudents + "</td>";
+    Table += "<td style='width: 100px; height: 30px;text-align: center;font-weight: bolder;background: lightblue;'>" + totalpass + "</td>";
+    Table += "<td style='width: 100px; height: 30px;text-align: center;font-weight: bolder;background: lightblue;'>" + totalfail + "</td>";
+    Table += "<td style='width: 100px; height: 30px;text-align: center;font-weight: bolder;background: lightblue;'>" + totalpasspercent + "</td></tr>";
     Table += "</table>";
 
     document.getElementById("Result").innerHTML = Table; 
 }
 
-function clear_complete()
-{
-    document.getElementById("ID").value="";
-    document.getElementById("grade").value="";
-    document.getElementsByName("div").value="";
-    document.getElementById("english").value="";
-    document.getElementById("maths").value="";
-    document.getElementById("science").value="";
-    document.getElementById("social").value="";
-}
-function clear_marks()
-{
-    document.getElementById("english").value="";
-    document.getElementById("maths").value="";
-    document.getElementById("science").value="";
-    document.getElementById("social").value="";    
-}
 
 
 
